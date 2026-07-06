3e5d9f30198a8e9888d50ead7265caabb1bcd57c
damianprog <damianpukszta@gmail.com>
2026-07-06 22:33:07 +0200

    DataStore isDefined type narrowing

--- data-layer-mike.ts
+++ data-layer-mike.ts
@@ -7,28 +7,45 @@ export interface Movie extends DataEntity {
 export interface Song extends DataEntity {
   singer: string;
 }
-export interface Comic extends DataEntity {
-  issueNumber: number;
-}
+// export interface Comic extends DataEntity {
+//   issueNumber: number;
+// }
 
 export type DataEntityMap = {
   movie: Movie;
   song: Song;
-  comic: Comic;
+  // comic: Comic;
 };
 
 type DataStoreMethods = {
-  [K in keyof DataEntityMap as `getAll${Capitalize<K>}s`]: 
-    () => DataEntityMap[K][];
+  [K in keyof DataEntityMap as `getAll${Capitalize<K>}s`]: () => DataEntityMap[K][];
 } & {
-    [K in keyof DataEntityMap as `get${Capitalize<K>}`]: 
-        (id: string) => DataEntityMap[K];
+  [K in keyof DataEntityMap as `get${Capitalize<K>}`]: (
+    id: string,
+  ) => DataEntityMap[K];
 } & {
-    [K in keyof DataEntityMap as `clear${Capitalize<K>}s`]: 
-        () => void;
-} ;
+  [K in keyof DataEntityMap as `clear${Capitalize<K>}s`]: () => void;
+};
 
-export class DataStore implements DataStoreMethods {}
+function isDefined<T>(x: T | undefined): x is T {
+  return typeof x !== "undefined";
+}
 
-const ds: DataStoreMethods = {} as any;
-ds.
+export class DataStore implements DataStoreMethods {
+  #data: { [K in keyof DataEntityMap]: Record<string, DataEntityMap[K]> } = {
+    movie: {},
+    song: {},
+  };
+
+  getAllSongs(): Song[] {
+    return Object.keys(this.#data.song)
+      .map((songKey) => this.#data.song[songKey])
+      .filter(isDefined);
+  }
+  getSong(): Song {}
+  clearSongs(): void {}
+
+  // getAllSongs(): Song[] {}
+  // getSong(): Song {}
+  // clearSongs(): void {}
+}
