a1f4780dbd05dc6c4461c31bf62eb4f3bbf2807e
damianprog <damianpukszta@gmail.com>
2026-07-07 18:03:28 +0200

    Data layer Mike solution complete

--- data-layer-mike.ts
+++ data-layer-mike.ts
@@ -25,6 +25,10 @@ type DataStoreMethods = {
   ) => DataEntityMap[K];
 } & {
   [K in keyof DataEntityMap as `clear${Capitalize<K>}s`]: () => void;
+} & {
+  [K in keyof DataEntityMap as `add${Capitalize<K>}`]: (
+    arg: DataEntityMap[K],
+  ) => DataEntityMap[K];
 };
 
 function isDefined<T>(x: T | undefined): x is T {
@@ -42,9 +46,35 @@ export class DataStore implements DataStoreMethods {
       .map((songKey) => this.#data.song[songKey])
       .filter(isDefined);
   }
-  getSong(): Song {}
-  clearSongs(): void {}
-
+  getSong(songKey: string): Song {
+    const song = this.#data.song[songKey];
+    if (!song) throw new Error(`Could not find song with id ${songKey}`);
+    return song;
+  }
+  clearSongs(): void {
+    this.#data.song = {};
+  }
+  getAllMovies(): Movie[] {
+    return Object.keys(this.#data.movie)
+      .map((movieKey) => this.#data.movie[movieKey])
+      .filter(isDefined);
+  }
+  getMovie(movieKey: string): Movie {
+    const movie = this.#data.movie[movieKey];
+    if (!movie) throw new Error(`Could not find Movie with id ${movieKey}`);
+    return movie;
+  }
+  clearMovies(): void {
+    this.#data.movie = {};
+  }
+  addMovie(m: Movie): Movie {
+    this.#data.movie[m.id] = m;
+    return m;
+  }
+  addSong(s: Song): Song {
+    this.#data.song[s.id] = s;
+    return s;
+  }
   // getAllSongs(): Song[] {}
   // getSong(): Song {}
   // clearSongs(): void {}
