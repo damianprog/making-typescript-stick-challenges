cbab2546a26712899017bc4119a63ce07f6ec1c7
damianprog <damianpukszta@gmail.com>
2026-09-02 13:42:03 +0200

    IndexOf type challenge

--- type-challenges/index-of.ts
+++ type-challenges/index-of.ts
@@ -10,9 +10,29 @@ type NotEqual<X, Y> = true extends Equal<X, Y> ? false : true;
 // ---cut---
 
 // Implement this type
-type IndexOf<T, U> = T extends [
-    ...infer head, infer tail
-] ? tail extends U ? head["length"] : IndexOf<head, U>
+// type IndexOf<T, U> = T extends [
+//     ...infer head, infer tail
+// ] ? tail extends U ? head["length"] : IndexOf<head, U>
+
+// [1, 2, 3]
+//    F = 1     Rest = [2, 3]
+//      IndexOf<[2, 3], 2, [1]>
+//      IndexOf<[3], 2, [1, 2]>
+
+// type IndexOf<T extends any[], U, Acc extends any[] = []> = T extends [
+//   infer F,
+//   ...infer Rest,
+// ]
+//   ? IndexOf<Rest, U, [...Acc, F]>
+//   : never;
+
+// let x: IndexOf<[1, 2, 3], 2>;
+
+type IndexOf<T extends any[], U, Acc extends any[] = []> = T[0] extends U
+  ? Acc["length"]
+  : T extends [infer F, ...infer Rest]
+    ? IndexOf<Rest, U, [...Acc, F]>
+    : -1;
 
 let x: IndexOf<[1, 2, 3], 2>;
 
